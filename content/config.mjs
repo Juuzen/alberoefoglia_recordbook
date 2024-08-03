da14b7dd9183943238331bf5b38c3bdcfe598b61
Michele Rega <Juuzen@users.noreply.github.com>
2024-08-03 22:11:39 +0200

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -2,6 +2,13 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {
-  showSidebar: true
+const config = {
+  title: "Cronache di Hydaelyn",
+  description: "Wiki per la campagna: Foglie dell'Albero Lucente",
+  author: "Michele Rega",
+  showSidebar: true,
+  // links to the pages you want to link to in the navbar and in the footer
+  //navLinks: [{ href: "/about", name: "About" }],
 };
+
+export default config;
