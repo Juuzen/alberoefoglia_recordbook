5dffee13bc4c3a69bcf68e897078a59a05e1a00c
Michele Rega <Juuzen@users.noreply.github.com>
2024-08-20 13:34:22 +0200

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -7,6 +7,9 @@ const config = {
   description: "Wiki per la campagna: Foglie dell'Albero Lucente",
   author: "Michele Rega",
   showSidebar: true,
+  search: {
+    provider: "kbar",
+  },
   // links to the pages you want to link to in the navbar and in the footer
   //navLinks: [{ href: "/about", name: "About" }],
 };
